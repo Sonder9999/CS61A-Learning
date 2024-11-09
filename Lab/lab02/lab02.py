b365e81696202e1a27bbad6ac104096f566c6b59
Sonder9999 <2734165908@qq.com>
2024-11-09 16:54:54 +0800

    添加实验2的测试文件

---
+++ Lab/lab02/lab02.py
@@ -0,0 +1,161 @@
+"""
+>>> not 10
+? False
+-- OK! --
+
+>>> not None
+? True
+-- OK! --
+
+>>> -1 and 1 > 0 # If this errors, just type Error.
+? True
+-- OK! --
+
+>>> -1 or 5
+? -1
+-- OK! --
+
+>>> print(3) or ""
+(line 1)? 3
+(line 2)? ''
+-- OK! --
+
+================
+
+>>> b = lambda x, y: lambda: x + y # Lambdas can return other lambdas!
+>>> c = b(8, 4)
+>>> c
+? Function
+-- OK! --
+>>> c()
+? 12
+-- OK! --
+
+>>> # Try drawing an environment diagram if you get stuck!
+>>> higher_order_lambda = lambda f: lambda x: f(x)
+>>> g = lambda x: x * x
+>>> higher_order_lambda(2)(g) # Which argument belongs to which function call?
+? Error
+-- OK! --
+
+>>> print_lambda = lambda z: print(z) # When is the return expression of a lambda expression executed?
+>>> print_lambda
+
+? Function
+-- OK! --
+
+由于 print 函数返回 None，所以 one_thousand 被赋值为 None。
+>>> one_thousand = print_lambda(1000)
+>>> one_thousand = print_lambda(1000)
+? 1000
+-- OK! --
+
+>>> one_thousand # What did the call to print_lambda return? If it displays nothing, write Nothing
+? Nothing
+-- OK! --
+"""
+
+
+def composite_identity(f, g):
+    """
+    Return a function with one parameter x that returns True if f(g(x)) is
+    equal to g(f(x)). You can assume the result of g(x) is a valid input for f
+    and vice versa.
+
+    >>> add_one = lambda x: x + 1        # adds one to x
+    >>> square = lambda x: x**2          # squares x [returns x^2]
+    >>> b1 = composite_identity(square, add_one)
+    >>> b1(0)                            # (0 + 1) ** 2 == 0 ** 2 + 1
+    True
+    >>> b1(4)                            # (4 + 1) ** 2 != 4 ** 2 + 1
+    False
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def sum_digits(y):
+    """Return the sum of the digits of non-negative integer y."""
+    total = 0
+    while y > 0:
+        total, y = total + y % 10, y // 10
+    return total
+
+
+def is_prime(n):
+    """Return whether positive integer n is prime."""
+    if n == 1:
+        return False
+    k = 2
+    while k < n:
+        if n % k == 0:
+            return False
+        k += 1
+    return True
+
+
+def count_cond(condition):
+    """Returns a function with one parameter N that counts all the numbers from
+    1 to N that satisfy the two-argument predicate function Condition, where
+    the first argument for Condition is N and the second argument is the
+    number from 1 to N.
+
+    >>> count_fives = count_cond(lambda n, i: sum_digits(n * i) == 5)
+    >>> count_fives(10)   # 50 (10 * 5)
+    1
+    >>> count_fives(50)   # 50 (50 * 1), 500 (50 * 10), 1400 (50 * 28), 2300 (50 * 46)
+    4
+
+    >>> is_i_prime = lambda n, i: is_prime(i) # need to pass 2-argument function into count_cond
+    >>> count_primes = count_cond(is_i_prime)
+    >>> count_primes(2)    # 2
+    1
+    >>> count_primes(3)    # 2, 3
+    2
+    >>> count_primes(4)    # 2, 3
+    2
+    >>> count_primes(5)    # 2, 3, 5
+    3
+    >>> count_primes(20)   # 2, 3, 5, 7, 11, 13, 17, 19
+    8
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def multiple(a, b):
+    """Return the smallest number n that is a multiple of both a and b.
+
+    >>> multiple(3, 4)
+    12
+    >>> multiple(14, 21)
+    42
+    """
+    "*** YOUR CODE HERE ***"
+
+
+def cycle(f1, f2, f3):
+    """Returns a function that is itself a higher-order function.
+
+    >>> def add1(x):
+    ...     return x + 1
+    >>> def times2(x):
+    ...     return x * 2
+    >>> def add3(x):
+    ...     return x + 3
+    >>> my_cycle = cycle(add1, times2, add3)
+    >>> identity = my_cycle(0)
+    >>> identity(5)
+    5
+    >>> add_one_then_double = my_cycle(2)
+    >>> add_one_then_double(1)
+    4
+    >>> do_all_functions = my_cycle(3)
+    >>> do_all_functions(2)
+    9
+    >>> do_more_than_a_cycle = my_cycle(4)
+    >>> do_more_than_a_cycle(2)
+    10
+    >>> do_two_cycles = my_cycle(6)
+    >>> do_two_cycles(1)
+    19
+    """
+    "*** YOUR CODE HERE ***"
